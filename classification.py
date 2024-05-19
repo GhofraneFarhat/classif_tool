3fc6e4e409c30e1c269f4232d6a53f6d76c4398f
GhofraneFARHAT <ghofrane.farhat@ensi-uma.tn>
2024-05-20 01:31:30 +0200

    tool changes to handle line command argument

--- classification.py
+++ classification.py
@@ -1,16 +1,15 @@
+import sys #handle command-line arguments
 import re
 from collections import defaultdict
 
-def classify(input_fasta):
-
-    output_fasta = "class.fasta"
+def classify(input_fasta, output_fasta):
     classifications = defaultdict(str)
-    
+
     with open(input_fasta, 'r') as file:
         for line in file:
             if line.startswith('>'):
                 contig_name = line.strip('>\n')
-                
+
                 # with names
                 if 'chromosome' in contig_name.lower():
                     classifications[contig_name] = 'chromosome'
@@ -18,12 +17,12 @@ def classify(input_fasta):
                     classifications[contig_name] = 'plasmid'
                 else:
                     classifications[contig_name] = 'ambiguous'
-    
+
     with open(output_fasta, 'w') as output_file:
         for contig, classification in classifications.items():
             output_file.write(f'>{contig} ({classification})\n')
-            
-            #result
+
+            # result
             with open(input_fasta, 'r') as input_file:
                 found_contig = False
                 for seq_line in input_file:
@@ -33,10 +32,15 @@ def classify(input_fasta):
                         output_file.write(seq_line)
                     elif found_contig and seq_line.startswith('>'):
                         break
-    
+
     return output_fasta
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_fasta> <output_fasta>")
+        sys.exit(1)
 
-input_fasta = "inputclass.fasta"
-output_fasta = classify(input_fasta)
-print(f'Results written to {output_fasta}')
+    input_fasta = sys.argv[1]
+    output_fasta = sys.argv[2]
+    classify(input_fasta, output_fasta)
+    print(f"Results written to {output_fasta}")
